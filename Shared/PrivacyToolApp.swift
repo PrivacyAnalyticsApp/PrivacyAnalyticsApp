fa8f6fe46257e74649ead725e603f9e6360f35cd
Henri <henribredt@web.de>
2022-07-26 09:39:43 +0200

    Initial Commit

---
+++ Shared/PrivacyToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrivacyToolApp.swift
+//  Shared
+//
+//  Created by Henri Bredt on 26.07.22.
+//
+
+import SwiftUI
+
+@main
+struct PrivacyToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
